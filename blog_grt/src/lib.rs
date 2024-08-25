1766fab8a823c693018c1ee757de8e89f8880f28
Joseph Hobbs <josephrosshobbs@gmail.com>
2024-08-25 16:09:19 -0400

    Update lints

--- blog_grt/src/lib.rs
+++ blog_grt/src/lib.rs
@@ -1,5 +1,11 @@
 //! Getroot utility for the Blog Builder.
 
+/// Enforce all warnings.
+#[deny(warnings)]
+
+/// Enforce all documentation.
+#[deny(missing_docs)]
+
 mod getroot;
 
 pub use getroot::getroot;
--- blog_str/src/lib.rs
+++ blog_str/src/lib.rs
@@ -1,5 +1,11 @@
 //! Site tree management for the Blog Builder.
 
+/// Enforce all warnings.
+#[deny(warnings)]
+
+/// Enforce all documentation.
+#[deny(missing_docs)]
+
 mod sitetree;
 
 pub use sitetree::SiteTree;
