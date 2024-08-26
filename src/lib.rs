7d5a5d906aeb6a2220026c596744fc3e85dcf733
Joseph Hobbs <josephrosshobbs@gmail.com>
2024-08-26 14:30:32 -0400

    Remove unneeded constants

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,6 @@
 // Enforce all documentation.
 #![deny(missing_docs)]
 
-/// Configuration file name.
-pub const CONFIG_FILE_NAME: &str = "blog.toml";
-
-/// Source directory name.
-pub const SOURCE_DIR_NAME: &str = "source";
-
-/// Output directory name.
-pub const OUTPUT_DIR_NAME: &str = "html";
-
-/// Source file extension.
-pub const SOURCE_FILE_EXT: &str = "txt";
-
-/// Output file extension.
-pub const OUTPUT_FILE_EXT: &str = "html";
-
 /// Command-line interface.
 pub mod cli {
     pub use blog_cli::{
